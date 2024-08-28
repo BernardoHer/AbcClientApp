460c52dab302f1c818fc083436ab7deca9f48285
Bernardo Hernández <109386038+BernardoHer@users.noreply.github.com>
2024-08-27 22:42:55 -0500

    Added required modifier to some properties

--- Models/ClientInfo.cs
+++ Models/ClientInfo.cs
@@ -2,15 +2,15 @@
 {
     public class ClientInfo
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; } = null!;
+        public required int Id { get; set; }
+        public required string FirstName { get; set; } = null!;
         public string? MiddleName { get; set; }
-        public string LastName { get; set; } = null!;
-        public string SecondLastName { get; set; } = null!;
-        public string ContactNumber { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public DateTime DateOfBirth { get; set; }
-        public decimal EstimatedInsuranceValue { get; set; }
+        public required string LastName { get; set; } = null!;
+        public required string SecondLastName { get; set; } = null!;
+        public required string ContactNumber { get; set; } = null!;
+        public required string Email { get; set; } = null!;
+        public required DateTime DateOfBirth { get; set; }
+        public required decimal EstimatedInsuranceValue { get; set; }
         public string? Notes { get; set; }
     }
 }
